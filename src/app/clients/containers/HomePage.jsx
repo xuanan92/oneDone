7ef9001ceaaa8aa298e2cb573d213e4fd1b46ede
xuanan92 <48396734+xuanan92@users.noreply.github.com>
2023-02-05 23:46:04 +0700

    update homepage

--- src/app/clients/containers/HomePage.jsx
+++ src/app/clients/containers/HomePage.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from "react"
 
 export default class HomePage extends Component {
-    constructor(props){
-    super(props)
-    
-    }
+    // constructor(props){
+    // super(props)
+    // 
+    // }
 
     render() {
     return(
