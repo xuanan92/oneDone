69ccf2969b99083e64c01c0642a999f6ca1a155b
root <root@XuanAnPC>
2023-04-02 19:36:53 +0700

    UI client update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import HomePage from './app/clients/containers/HomePage'
-import Register from "./app/clients/components/Register" 
-import Navbar from "./app/clients/components/Navbar" 
-import Footer from "./app/clients/components/Footer" 
-import Login from "./app/clients/components/Login" 
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import Register from "./app/clients/components/Register"
+import Navbar from "./app/clients/components/Navbar"
+import Footer from "./app/clients/components/Footer"
+import Login from "./app/clients/components/Login"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
 
 export default function App() {
@@ -13,9 +13,13 @@ export default function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element = {<HomePage />} />
-          <Route path="/register" element = {<Register />} />
-          <Route path="/login" element = {<Login />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          {/* <Route path="/aboutus" element={<Login />} /> */}
+          {/* <Route path="/products" element={<Login />} /> */}
+          {/* <Route path="/blogs" element={<Login />} /> */}
+          {/* <Route path="/contact" element={<Login />} /> */}
         </Routes>
         <Footer />
       </Router>
--- src/app/clients/components/Login.jsx
+++ src/app/clients/components/Login.jsx
@@ -1,74 +1,80 @@
 import React, { Component } from "react"
+import { Link } from "react-router-dom"
 
 export default class Name extends Component {
-    constructor(props){
+  constructor(props) {
     super(props)
-        // this.handleSubmit= this.handleSubmit.bind(this)
-        this.handleChange= this.handleChange.bind(this)
-        this.state = {
-            username: '',
-            password: ''
-        }
+    // this.handleSubmit= this.handleSubmit.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+    this.state = {
+      username: '',
+      password: ''
     }
+  }
 
 
-    handleChange(event){
-        this.setState({
-            [event.target.name] : event.target.value,
-        })
-    }
-    render() {
-    return(
-        <>
-            <div className="p-2 bg-gray-500">
-                <div className="text-center">
-                    <h1 className="p-2">Login here!</h1>
-                </div>
-                <table className="m-auto">
-                    <thead>
-                        <th></th>
-                        <th></th>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td className="p-2">Name:</td>
-                            <td className="p-2">
-                                <input
-                                    type="text"
-                                    name="username"
-                                    className="p-2 border rounded"
-                                    value={this.state.name}
-                                    onChange={this.handleChange}
-                                    />
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="p-2">Password:</td>
-                            <td className="p-2">
-                                <input
-                                    type="text"
-                                    name="username"
-                                    className="p-2 border rounded"
-                                    value={this.state.name}
-                                    onChange={this.handleChange}
-                                    />
-                            </td>
-                        </tr>
-                        <tr>
-                            <td></td>
-                            <td className="text-center font-bold">
-                                <input
-                                    type="submit"
-                                    name=""
-                                    className=""
-                                    value="Submit"
-                                />
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </>
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value,
+    })
+  }
+  render() {
+    return (
+      <>
+        <div className="p-2 bg-[var(--foregr)]">
+          <div className="text-center">
+            <h1 className="p-2">Login here!</h1>
+          </div>
+          <table className="m-auto">
+            <thead>
+              <th></th>
+              <th></th>
+            </thead>
+            <tbody>
+              <tr>
+                <td className="p-2">Username:</td>
+                <td className="p-2">
+                  <input
+                    type="text"
+                    name="username"
+                    className="p-2 border rounded border-black"
+                    value={this.state.name}
+                    onChange={this.handleChange}
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td className="p-2">Password:</td>
+                <td className="p-2">
+                  <input
+                    type="text"
+                    name="username"
+                    className="p-2 border rounded border-black"
+                    value={this.state.name}
+                    onChange={this.handleChange}
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td></td>
+                <td className="text-center font-bold">
+                  <input
+                    type="submit"
+                    name=""
+                    className="p-2 rounded border border-gray-400"
+                    value="Submit"
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td colspan="2">
+                  <p className="p-2">Don't have account?<Link to="/register"> Register here</Link></p>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </>
     )
-}
+  }
 }
